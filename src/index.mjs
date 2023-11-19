ee2f2b7a3f339c6d6a7c9c46dcf77251570db3d1
Plumnet <tomomi0131@ymail.ne.jp>
2023-11-19 11:11:38 +0900

    liタグDOM生成確認

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,11 @@ const onClickAdd = () => {
   //テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
+
+// li作成
+  const li = document.createElement("li");
+  console.log(li);
 }
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
